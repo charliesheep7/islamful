ab33211b7372deccb9dae11579c872fa244a410d
charliesheep7 <sheepcharlie7@gmail.com>
2025-10-23 00:49:47 -0700

    Add sitemap debug logging to diagnose missing SEObot posts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,12 +25,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   // Fetch SEObot posts and add to sitemap (English only)
   const seoBotPosts = await getSeoBotPosts()
+  console.log(`[SITEMAP] Fetched ${seoBotPosts.length} SEObot posts`)
+  seoBotPosts.forEach((post) => {
+    console.log(`[SITEMAP] SEObot post: slug=${post.slug}, draft=${post.draft}, path=${post.path}`)
+  })
   const seoBotRoutes = seoBotPosts
     .filter((post) => !post.draft)
     .map((post) => ({
       url: `${siteUrl}/${post.path}`,
       lastModified: post.lastmod || post.date,
     }))
+  console.log(`[SITEMAP] After draft filter: ${seoBotRoutes.length} SEObot routes`)
 
   const routes = ['', 'blog', 'ar', 'ar/blog'].map((route) => ({
     url: `${siteUrl}/${route}`,
