435830d2686f4bbfb2dabf1eacf010d1221f60e7
charliesheep7 <sheepcharlie7@gmail.com>
2025-11-07 23:08:35 -0800

    new push

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -118,6 +118,7 @@ export const Blog = defineDocumentType(() => ({
     layout: { type: 'string' },
     bibliography: { type: 'string' },
     canonicalUrl: { type: 'string' },
+    faqs: { type: 'json' },
   },
   computedFields: {
     ...computedFields,
