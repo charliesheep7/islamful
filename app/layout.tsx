a213fcd59ff5989f1885bfa401a2ba0f8914411a
charliesheep7 <sheepcharlie7@gmail.com>
2025-11-10 01:43:23 -0800

    ne wfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { Noto_Sans, Noto_Sans_Arabic, DM_Serif_Text } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
-import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
@@ -148,7 +147,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
             <Header />
             <main className="mb-auto">{children}</main>
-            <Footer />
           </SearchProvider>
         </ThemeProviders>
       </body>
